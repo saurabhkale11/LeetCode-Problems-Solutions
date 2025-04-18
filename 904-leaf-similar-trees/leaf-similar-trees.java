1c2abba586d8289b147b6f4e01c8ac0ef1075d46
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-04-18 07:18:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (80.07%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.java
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -1,33 +1,24 @@
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        return getLeafSequence(root1).equals(getLeafSequence(root2));
+        List<Integer> leaves1 = new ArrayList<>();
+        List<Integer> leaves2 = new ArrayList<>();
+        
+        getLeaves(root1, leaves1);
+        getLeaves(root2, leaves2);
+        
+        return leaves1.equals(leaves2);
     }
     
-    private List<Integer> getLeafSequence(TreeNode root) {
-        List<Integer> leafSequence = new ArrayList<>();
-        traverseTree(root, leafSequence);
-        return leafSequence;
-    }
-    
-    private void traverseTree(TreeNode root, List<Integer> leafSequence) {
-        if (root == null) return;
-        if (root.left == null && root.right == null) {
-            leafSequence.add(root.val);
+    private void getLeaves(TreeNode root, List<Integer> leaves) {
+        if (root == null) {
+            return;
         }
-        traverseTree(root.left, leafSequence);
-        traverseTree(root.right, leafSequence);
+        
+        if (root.left == null && root.right == null) {
+            leaves.add(root.val);
+        }
+        
+        getLeaves(root.left, leaves);
+        getLeaves(root.right, leaves);
     }
 }
