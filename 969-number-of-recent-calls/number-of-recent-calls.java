a5a4b11d203db5229e6eb126c4a51c0d5fa7adc7
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-04-18 07:01:55 +0530

    Time: 20 ms (84.08%) | Memory: 54.6 MB (27.72%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.java
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -1,19 +1,17 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
-public class RecentCounter {
-    private Queue<Integer> queue;
+class RecentCounter {
+    Queue<Integer> queue;
 
     public RecentCounter() {
         queue = new LinkedList<>();
     }
 
     public int ping(int t) {
-        queue.offer(t);  // Add the new request
-        // Remove requests that are outside the 3000 ms window
+        queue.add(t);
         while (queue.peek() < t - 3000) {
             queue.poll();
         }
-        return queue.size();  // Return the number of requests in the window
+        return queue.size();
     }
 }
