9ed574dc6da32563f3bd725aa5133e61a4c1092c
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-04-19 09:16:19 +0530

    Time: 765 ms (90.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1390-average-selling-price/average-selling-price.sql
@@ -0,0 +1,16 @@
+SELECT 
+    u.product_id,
+    ROUND(SUM(u.units * p.price) * 1.0 / SUM(u.units), 2) AS average_price
+FROM UnitsSold u
+JOIN Prices p
+  ON u.product_id = p.product_id
+ AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY u.product_id
+UNION
+SELECT 
+    p.product_id,
+    0.00 AS average_price
+FROM Prices p
+WHERE p.product_id NOT IN (
+    SELECT DISTINCT product_id FROM UnitsSold
+);
