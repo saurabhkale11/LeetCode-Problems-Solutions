1e3a40a85b877ecd2ce7aa77d007c387ab7e6e35
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-04-22 10:09:42 +0530

    Time: 1 ms (98.24%) | Memory: 44.6 MB (77.82%) - LeetSync

---
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.java
@@ -0,0 +1,41 @@
+import java.util.*;  // import statement at the top of the file
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> trees = new ArrayList<>();
+        if (start > end) {
+            trees.add(null);
+            return trees;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    trees.add(root);
+                }
+            }
+        }
+        return trees;
+    }
+}
