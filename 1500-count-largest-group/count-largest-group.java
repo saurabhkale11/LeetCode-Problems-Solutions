8d750517832a045c7c89c819da77ac8514728b13
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-04-23 06:43:30 +0530

    Time: 13 ms (35.13%) | Memory: 41.7 MB (13.98%) - LeetSync

--- 1500-count-largest-group/count-largest-group.java
+++ 1500-count-largest-group/count-largest-group.java
@@ -4,13 +4,15 @@ public class Solution {
     public int countLargestGroup(int n) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 1; i <= n; i++) {
-            int sum = digitSum(i);
+            int sum = getDigitSum(i);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
 
         int maxSize = 0;
         for (int size : map.values()) {
-            maxSize = Math.max(maxSize, size);
+            if (size > maxSize) {
+                maxSize = size;
+            }
         }
 
         int count = 0;
@@ -23,9 +25,9 @@ public class Solution {
         return count;
     }
 
-    private int digitSum(int num) {
+    private int getDigitSum(int num) {
         int sum = 0;
-        while (num > 0) {
+        while (num != 0) {
             sum += num % 10;
             num /= 10;
         }
