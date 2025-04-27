766a3183b58572fe41f9a457a73653026f161d45
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-04-27 08:25:03 +0530

    Time: 5 ms (52.59%) | Memory: 44.9 MB (36.35%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,29 +1,28 @@
-import java.util.*;
-
 public class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+        // Roman numeral values
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
 
         int total = 0;
-        int prev = 0;
-
-        for (int i = s.length() - 1; i >= 0; i--) {
-            int curr = map.get(s.charAt(i));
-            if (curr < prev) {
-                total -= curr;
+        for (int i = 0; i < s.length(); i++) {
+            // Get the value of the current Roman numeral
+            int currentVal = romanMap.get(s.charAt(i));
+            
+            // If this is not the last character and the next character is larger, subtract the current value
+            if (i + 1 < s.length() && currentVal < romanMap.get(s.charAt(i + 1))) {
+                total -= currentVal;
             } else {
-                total += curr;
+                total += currentVal;
             }
-            prev = curr;
         }
-
+        
         return total;
     }
 }
