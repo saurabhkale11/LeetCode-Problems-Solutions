33c221eb7079d77d08b7eb58c19fe75e05daf306
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-04-28 12:14:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (88.74%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,15 +1,11 @@
-public class Solution {
+class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-
-    private boolean isValidBST(TreeNode root, long min, long max) {
-        if (root == null) {
-            return true;
-        }
-        if (root.val <= min || root.val >= max) {
-            return false;
-        }
-        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    
+    private boolean helper(TreeNode node, long lower, long upper) {
+        if (node == null) return true;
+        if (node.val <= lower || node.val >= upper) return false;
+        return helper(node.left, lower, node.val) && helper(node.right, node.val, upper);
     }
 }
