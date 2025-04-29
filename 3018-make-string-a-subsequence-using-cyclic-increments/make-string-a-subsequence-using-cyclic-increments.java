d15895818f33cfe83a63893f776cbeb42e8b65de
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-04-29 07:40:04 +0530

    Time: 8 ms (13.85%) | Memory: 45.7 MB (48.61%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
@@ -1,14 +1,42 @@
 public class Solution {
     public boolean canMakeSubsequence(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        // Check if str2 is a subsequence of str1 without any operations
         int i = 0, j = 0;
-        while (i < str1.length() && j < str2.length()) {
-            char c1 = str1.charAt(i);
-            char c2 = str2.charAt(j);
-            if (c1 == c2 || (char)((c1 - 'a' + 1) % 26 + 'a') == c2) {
+        while (i < n && j < m) {
+            if (str1.charAt(i) == str2.charAt(j)) {
                 j++;
             }
             i++;
         }
-        return j == str2.length();
+
+        // If str2 is already a subsequence of str1
+        if (j == m) {
+            return true;
+        }
+
+        // Now check if we can transform str1 to match str2 using at most one operation
+        i = 0;
+        j = 0;
+        boolean canMake = false;
+
+        while (i < n && j < m) {
+            char char1 = str1.charAt(i);
+            char char2 = str2.charAt(j);
+            
+            if (char1 == char2) {
+                // Characters match, move both pointers
+                j++;
+            } else if ((char1 + 1 - 'a') % 26 == (char2 - 'a') % 26) {
+                // Check if we can increment char1 to match char2
+                canMake = true;
+                j++;
+            }
+            i++;
+        }
+
+        return canMake && j == m;  // Check if all characters of str2 have been matched
     }
 }
