d16ea70f6c77527f0be48964a06e9cdcb5b348c0
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-04-29 07:42:30 +0530

    Time: 62 ms (17.00%) | Memory: 46.1 MB (31.83%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -1,20 +1,36 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
     public int maxCount(int[] banned, int n, int maxSum) {
+        // Convert banned array to a set for faster lookup
         Set<Integer> bannedSet = new HashSet<>();
         for (int num : banned) {
             bannedSet.add(num);
         }
-        int count = 0;
-        int sum = 0;
+
+        // Collect all integers in range [1, n] that are not in bannedSet
+        List<Integer> validNumbers = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
-            if (bannedSet.contains(i)) continue;
-            if (sum + i > maxSum) break;
-            sum += i;
-            count++;
+            if (!bannedSet.contains(i)) {
+                validNumbers.add(i);
+            }
         }
+
+        // Sort valid numbers to pick the smallest ones first
+        Collections.sort(validNumbers);
+
+        // Try to select the numbers in increasing order until maxSum is exceeded
+        int sum = 0;
+        int count = 0;
+        for (int num : validNumbers) {
+            if (sum + num <= maxSum) {
+                sum += num;
+                count++;
+            } else {
+                break;
+            }
+        }
+
         return count;
     }
 }
