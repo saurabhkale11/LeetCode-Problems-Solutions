86cbd630d10c6a68c641a0035f5de72d3a50573f
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-04-30 08:53:52 +0530

    Time: 11 ms (72.88%) | Memory: 45.8 MB (49.08%) - LeetSync

--- 649-dota2-senate/dota2-senate.java
+++ 649-dota2-senate/dota2-senate.java
@@ -1,26 +1,29 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public String predictPartyVictory(String senate) {
+        int n = senate.length();
         Queue<Integer> radiant = new LinkedList<>();
         Queue<Integer> dire = new LinkedList<>();
-        int n = senate.length();
+
         for (int i = 0; i < n; i++) {
             if (senate.charAt(i) == 'R') {
-                radiant.add(i);
+                radiant.offer(i);
             } else {
-                dire.add(i);
+                dire.offer(i);
             }
         }
+
         while (!radiant.isEmpty() && !dire.isEmpty()) {
-            int r = radiant.poll();
-            int d = dire.poll();
-            if (r < d) {
-                radiant.add(r + n);
+            int rIndex = radiant.poll();
+            int dIndex = dire.poll();
+            if (rIndex < dIndex) {
+                radiant.offer(rIndex + n);
             } else {
-                dire.add(d + n);
+                dire.offer(dIndex + n);
             }
         }
+
         return radiant.isEmpty() ? "Dire" : "Radiant";
     }
 }
