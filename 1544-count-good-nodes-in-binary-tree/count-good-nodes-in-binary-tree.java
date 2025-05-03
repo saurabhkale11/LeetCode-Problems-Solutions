aa138720746fc2fd8e99d4a7bcafc1aa4967e402
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-05-03 09:18:39 +0530

    Time: 2 ms (85.63%) | Memory: 52.6 MB (34.21%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -1,23 +1,12 @@
 class Solution {
     public int goodNodes(TreeNode root) {
-        return dfs(root, Integer.MIN_VALUE);
+        return dfs(root, root.val);
     }
-    
-    private int dfs(TreeNode node, int maxVal) {
-        if (node == null) {
-            return 0;
-        }
-        
-        int goodNodeCount = 0;
-        if (node.val >= maxVal) {
-            goodNodeCount = 1;
-        }
-        
-        maxVal = Math.max(maxVal, node.val);
-        
-        goodNodeCount += dfs(node.left, maxVal);
-        goodNodeCount += dfs(node.right, maxVal);
-        
-        return goodNodeCount;
+
+    private int dfs(TreeNode node, int maxSoFar) {
+        if (node == null) return 0;
+        int good = node.val >= maxSoFar ? 1 : 0;
+        int newMax = Math.max(maxSoFar, node.val);
+        return good + dfs(node.left, newMax) + dfs(node.right, newMax);
     }
 }
