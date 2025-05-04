c91c004149d8ccd44d799dac6260fe6043dc70f8
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-05-04 07:07:19 +0530

    Time: 3 ms (54.26%) | Memory: 43.7 MB (59.22%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,29 +1,22 @@
-import java.util.*;
-
-public class Solution {
+class Solution {
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
-        int[] answer = new int[n];
+        int[] result = new int[n];
         Stack<Integer> stack = new Stack<>();
-        
-        for (int i = n - 1; i >= 0; i--) {
-            // Pop all elements from the stack that are greater than the current price
-            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                stack.pop();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                int idx = stack.pop();
+                result[idx] = prices[idx] - prices[i];
             }
-            
-            // If stack is not empty, the element at the top of the stack is the discount
-            if (!stack.isEmpty()) {
-                answer[i] = prices[i] - prices[stack.peek()];
-            } else {
-                // No discount
-                answer[i] = prices[i];
-            }
-            
-            // Push the current index onto the stack
             stack.push(i);
         }
-        
-        return answer;
+
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            result[idx] = prices[idx];
+        }
+
+        return result;
     }
 }
