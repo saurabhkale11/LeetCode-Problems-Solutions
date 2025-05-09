1f346f8cb477030ce19eb6757ad564349ffc8bdb
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-05-09 07:51:25 +0530

    Time: 1 ms (88.85%) | Memory: 44.5 MB (13.73%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -1,26 +1,23 @@
 public class Solution {
     public int[] applyOperations(int[] nums) {
-        // Apply operations
-        for (int i = 0; i < nums.length - 1; i++) {
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
             if (nums[i] == nums[i + 1]) {
                 nums[i] *= 2;
                 nums[i + 1] = 0;
             }
         }
 
-        // Move zeros to the end
-        int nonZeroIndex = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[nonZeroIndex++] = nums[i];
+        int[] result = new int[n];
+        int index = 0;
+
+        for (int num : nums) {
+            if (num != 0) {
+                result[index++] = num;
             }
         }
-        
-        // Fill remaining positions with 0
-        for (int i = nonZeroIndex; i < nums.length; i++) {
-            nums[i] = 0;
-        }
 
-        return nums;
+        return result;
     }
 }
