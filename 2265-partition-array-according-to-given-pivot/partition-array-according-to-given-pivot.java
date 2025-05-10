6332c3ead60ae2ada1c5b763ac8280aaaf9642c9
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-05-10 18:43:58 +0530

    Time: 18 ms (10.29%) | Memory: 62.9 MB (80.58%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,33 +1,22 @@
 public class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        // Lists to store elements less than, equal to, and greater than pivot
         List<Integer> less = new ArrayList<>();
         List<Integer> equal = new ArrayList<>();
         List<Integer> greater = new ArrayList<>();
-        
-        // Categorize elements based on their relation to pivot
+
         for (int num : nums) {
-            if (num < pivot) {
-                less.add(num);
-            } else if (num == pivot) {
-                equal.add(num);
-            } else {
-                greater.add(num);
-            }
+            if (num < pivot) less.add(num);
+            else if (num == pivot) equal.add(num);
+            else greater.add(num);
         }
-        
-        // Concatenate the lists: less, equal, greater
-        List<Integer> result = new ArrayList<>();
-        result.addAll(less);
-        result.addAll(equal);
-        result.addAll(greater);
-        
-        // Convert the result list to an array and return it
-        int[] resArray = new int[nums.length];
-        for (int i = 0; i < result.size(); i++) {
-            resArray[i] = result.get(i);
-        }
-        
-        return resArray;
+
+        int[] result = new int[nums.length];
+        int index = 0;
+
+        for (int num : less) result[index++] = num;
+        for (int num : equal) result[index++] = num;
+        for (int num : greater) result[index++] = num;
+
+        return result;
     }
 }
