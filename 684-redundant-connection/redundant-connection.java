38bd32be40b4743c8435e9b51e33b583cf575aa6
Saurabh Kale <101178695+saurabhkale11@users.noreply.github.com>
2025-05-13 08:09:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (73.24%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,66 +1,23 @@
 public class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
-        UnionFind uf = new UnionFind(n + 1);  // We use n + 1 to accommodate 1-indexing
+        int[] parent = new int[n + 1];
 
-        // Traverse edges in the input order
-        for (int i = 0; i < n; i++) {
-            int[] edge = edges[i];
-            int u = edge[0];
-            int v = edge[1];
+        for (int i = 0; i <= n; i++) parent[i] = i;
 
-            if (uf.find(u) == uf.find(v)) {
-                // If u and v are already connected, this is the redundant edge
-                return edge;
-            }
-            uf.union(u, v);
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            int pu = find(parent, u);
+            int pv = find(parent, v);
+            if (pu == pv) return edge;
+            parent[pu] = pv;
         }
 
-        // If no cycle is found (shouldn't happen as we know one edge creates a cycle)
-        return new int[]{};
+        return new int[0];
     }
 
-    // Union-Find data structure
-    class UnionFind {
-        int[] parent;
-
-        UnionFind(int n) {
-            parent = new int[n];
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-            }
-        }
-
-        // Find operation with path compression
-        public int find(int x) {
-            if (parent[x] != x) {
-                parent[x] = find(parent[x]);  // Path compression
-            }
-            return parent[x];
-        }
-
-        // Union operation
-        public void union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-
-            if (rootX != rootY) {
-                parent[rootX] = rootY;  // Union the two components
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-
-        // Example 1
-        int[][] edges1 = {{1,2},{1,3},{2,3}};
-        int[] result1 = sol.findRedundantConnection(edges1);
-        System.out.println("Redundant edge: [" + result1[0] + "," + result1[1] + "]"); // Expected: [2,3]
-
-        // Example 2
-        int[][] edges2 = {{1,2},{2,3},{3,4},{1,4},{1,5}};
-        int[] result2 = sol.findRedundantConnection(edges2);
-        System.out.println("Redundant edge: [" + result2[0] + "," + result2[1] + "]"); // Expected: [1,4]
+    private int find(int[] parent, int x) {
+        if (parent[x] != x) parent[x] = find(parent, parent[x]);
+        return parent[x];
     }
 }
